6cfa38d609f3cc0841f14106199b5ec7df2afea4
Sergey Maksimov <s.mksimov@gmail.com>
2023-01-09 13:25:21 +0300

    add draft layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,32 @@ function App() {
 
   return (
     <div className={s.app}>
-     hello
+      <div>
+        <header>
+          <h3>Отдаете</h3>
+        </header>
+        <div className={s.flex}>
+          <h4>Все</h4>
+          <h4>Криптовалюты</h4>
+          <h4>Наличные</h4>
+          <h4>Банки RUB</h4>
+          <h4>Банки UAH</h4>
+        </div>
+        <input type="text" /><select></select>
+      </div>
+      <div>
+        <header>
+          <h3>Получаете</h3>
+        </header>
+        <div className={s.flex}>
+          <h4>Все</h4>
+          <h4>Криптовалюты</h4>
+          <h4>Банки RUB</h4>
+          <h4>Банки UAH</h4>
+          <h4>Наличные</h4>
+        </div>
+        <input type="text" /><select></select>
+      </div>
     </div>
   )
 }
