63de00f938bcf2b716cb940a1828302ae6acf226
Sergey Maksimov <s.mksimov@gmail.com>
2023-01-24 12:36:33 +0300

    fix PanelFrom

--- src/vidgets/PanelFrom/PanelFrom.tsx
+++ src/vidgets/PanelFrom/PanelFrom.tsx
@@ -1,14 +1,7 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { FilterType } from '../../App';
+import { useSelector } from 'react-redux';
 import { DirectionSelect } from '../../features';
 import { FromFilterButtons } from '../../features/FromFilterButtons';
-import {
-  getCategories,
-  getCategoryIds,
-  getCurrentCategoryFrom,
-  getDirectionsFrom,
-} from '../../store/directionSelectors';
-import { setCurrentCategoryFrom } from '../../store/directionSlice';
+import { getDirectionsFrom } from '../../store/directionSelectors';
 import s from './PanelFrom.module.scss';
 
 export function PanelFrom() {
@@ -20,12 +13,10 @@ export function PanelFrom() {
 
   return (
     <>
-      <header>
-        <h3 className={s.header}>Отдаете</h3>
+      <header className={s.header}>
+        <h3>Отдаете</h3>
       </header>
-      <div className={s.buttons}>
-        <FromFilterButtons />
-      </div>
+      <FromFilterButtons />
       <div className={s.select}>
         <input type="text" />
         <DirectionSelect optionIds={directionsFrom} onChange={handleChange} />
