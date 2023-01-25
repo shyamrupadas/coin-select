54e0e324b7775bd88519ce84ff871b352843fd53
Sergey Maksimov <s.mksimov@gmail.com>
2023-01-25 09:28:51 +0300

    refactor App

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,9 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import {
-  filteredCategoriesFromSelector,
-  filteredCategoriesToSelector,
-} from './store/categorySelectors';
-import {
-  setCurrentCategoryFrom,
-  setCurrentCategoryTo,
-  setData,
-} from './store/directionSlice';
+import { setData } from './store/directionSlice';
 import { data } from './api';
-import {
-  getCategories,
-  getCategoryIds,
-  getCurrentCategoryFrom,
-  getCurrentCategoryTo,
-  getCurrentDirectionFrom,
-  getFilteredOptionsFrom,
-} from './store/directionSelectors';
 import { PanelFrom } from './vidgets';
 import { PanelTo } from './vidgets/PanelTo';
 
-const filterMap: any = {
-  all: [],
-  crypto: ['BTC', 'ETH', 'USDTTRC'],
-  bank: ['ACRUB', 'SBERRUB', 'TCSRUB'],
-  cash: ['CASHUSD', 'CASHRUB'],
-};
-
 export type FilterType = 'all' | 'crypto' | 'bank' | 'cash';
-
-interface IButton {
-  id: FilterType;
-  label: string;
-}
 
 export function App() {
   const dispatch = useDispatch();
